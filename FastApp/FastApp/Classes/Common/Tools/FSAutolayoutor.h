734300037f1170742fd6bb6c0bbb372afe04ef74
tangkunyin <sir@shuoit.net>
2016-04-18 14:23:40 +0800

    重构布局工具

--- FastApp/FastApp/Classes/Common/Tools/FSAutolayoutor.h
+++ FastApp/FastApp/Classes/Common/Tools/FSAutolayoutor.h
@@ -18,58 +18,46 @@ typedef NS_ENUM(NSInteger,FSLayoutAlignmentType)
     AlignmentRight
 };
 
-//使用Masonry库封装的自动布局相关工具。支持旋屏
+
+/**
+ *  使用Masonry库封装的自动布局相关工具，自适应宽高（可指定最小宽高），支持旋屏
+ *  其中：部分方法需要设置子控件大小(CGSize)值，否则布局系统则会抛出异常
+ */
 @interface FSAutolayoutor : NSObject
 
 
+//以下三个方法均为中心定位
 + (void)layView:(UIView *)view fullOfTheView:(UIView *)superView;
 + (void)layView:(UIView *)view atCenterOfTheView:(UIView *)superView;
 + (void)layView:(UIView *)view atCenterOfTheView:(UIView *)superView margins:(float)margins;
 
 
+//以下三个方法均为左定位（左上，左中，左下）
++ (void)layView:(UIView *)subview atTheLeftTopOfTheView:(UIView *)container offset:(CGSize)offset;
++ (void)layView:(UIView *)subview atTheLeftMiddleOfTheView:(UIView *)container offset:(float)offset;
++ (void)layView:(UIView *)subview atTheLeftBottomOfTheView:(UIView *)container offset:(CGSize)offset;
+
+//以下三个方法均为右定位（右上，右中，右下）
++ (void)layView:(UIView *)subview atTheRightTopOfTheView:(UIView *)container offset:(CGSize)offset;
++ (void)layView:(UIView *)subview atTheRightMiddleOfTheView:(UIView *)container offset:(float)offset;
++ (void)layView:(UIView *)subview atTheRightBottomOfTheView:(UIView *)container offset:(CGSize)offset;
+
+//以下二个方法为上下定位（中上，中下）
++ (void)layView:(UIView *)subview atTheTopMiddleOfTheView:(UIView *)container offset:(float)offset;
++ (void)layView:(UIView *)subview atTheBottomMiddleOfTheView:(UIView *)container offset:(float)offset;
 
 
-+ (void)layView:(UIView*)subview atTheLeftMiddleOfTheView:(UIView*)container offset:(float)offset;
-+ (void)layView:(UIView*)subview atTheRightMiddleOfTheView:(UIView*)container offset:(float)offset;
-+ (void)layView:(UIView*)subview atTheTopMiddleOfTheView:(UIView*)container offset:(float)offset;
-+ (void)layView:(UIView*)subview atTheBottomMiddleOfTheView:(UIView*)container offset:(float)offset;
+//以下方法均为外部定位，如果不指定type值，则默认在目标控件中间位置
++ (void)layView:(UIView *)sourceView toTheRightOfTheView:(UIView *)targetView span:(float)span;
++ (void)layView:(UIView *)sourceView toTheRightOfTheView:(UIView *)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType;
 
-+ (void)layView:(UIView*)subview atTheLeftTopOfTheView:(UIView*)container offset:(CGSize)offset;
-+ (void)layView:(UIView*)subview atTheRightTopOfTheView:(UIView*)container offset:(CGSize)offset;
-+ (void)layView:(UIView*)subview atTheLeftBottomOfTheView:(UIView*)container offset:(CGSize)offset;
-+ (void)layView:(UIView*)subview atTheRightBottomOfTheView:(UIView*)container offset:(CGSize)offset;
++ (void)layView:(UIView *)sourceView toTheLeftOfTheView:(UIView *)targetView span:(float)span;
++ (void)layView:(UIView *)sourceView toTheLeftOfTheView:(UIView *)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType;
 
-+ (void)layView:(UIView *)sourceView toTheRightOfTheView:(UIView*)targetView span:(float)span;
-+ (void)layView:(UIView *)sourceView toTheLeftOfTheView:(UIView*)targetView span:(float)span;
-+ (void)layView:(UIView *)sourceView aboveTheView:(UIView*)targetView span:(float)span;
-+ (void)layView:(UIView *)sourceView belowTheView:(UIView*)targetView span:(float)span;
++ (void)layView:(UIView *)sourceView aboveTheView:(UIView *)targetView span:(float)span;
++ (void)layView:(UIView *)sourceView aboveTheView:(UIView *)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType;
 
-+ (void)layView:(UIView *)sourceView toTheRightOfTheView:(UIView*)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType;
-+ (void)layView:(UIView *)sourceView toTheLeftOfTheView:(UIView*)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType;
-+ (void)layView:(UIView *)sourceView aboveTheView:(UIView*)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType;
-+ (void)layView:(UIView *)sourceView belowTheView:(UIView*)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType;
-
-
-/**
- *  根据位置类型，在targetView内放置sourceView。targetView与sourceView为父子关系
- *
- *  @param sourceView 源视图
- *  @param targetView 目标视图
- *  @param type       位置类型
- *  @param size       最大值
- *  @param offset     距离位置类型处的横纵向距离
- */
-
-/**
- *  根据位置类型，在targetView外放置sourceView。targetView与sourceView为平级关系
- *
- *  @param sourceView 源视图
- *  @param targetView 目标视图
- *  @param type       位置类型
- *  @param size       最大值
- *  @param offset     距离位置类型处的横纵向距离
- */
-
-
++ (void)layView:(UIView *)sourceView belowTheView:(UIView *)targetView span:(float)span;
++ (void)layView:(UIView *)sourceView belowTheView:(UIView *)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType;
 
 @end
--- FastApp/FastApp/Classes/Common/Tools/FSAutolayoutor.m
+++ FastApp/FastApp/Classes/Common/Tools/FSAutolayoutor.m
@@ -10,112 +10,99 @@
 
 @implementation FSAutolayoutor
 
+//中间定位
++ (void)layView:(UIView *)view fullOfTheView:(UIView *)superView
+{
+
+}
++ (void)layView:(UIView *)view atCenterOfTheView:(UIView *)superView
+{
+
+}
++ (void)layView:(UIView *)view atCenterOfTheView:(UIView *)superView margins:(float)margins
+{
+
+}
 
 
+//左边定位
++ (void)layView:(UIView *)subview atTheLeftTopOfTheView:(UIView *)container offset:(CGSize)offset
+{
+
+}
++ (void)layView:(UIView *)subview atTheLeftMiddleOfTheView:(UIView *)container offset:(float)offset
+{
+
+}
++ (void)layView:(UIView *)subview atTheLeftBottomOfTheView:(UIView *)container offset:(CGSize)offset
+{
+
+}
+
+//右边定位
++ (void)layView:(UIView *)subview atTheRightTopOfTheView:(UIView *)container offset:(CGSize)offset
+{
+
+}
++ (void)layView:(UIView *)subview atTheRightMiddleOfTheView:(UIView *)container offset:(float)offset
+{
+
+}
++ (void)layView:(UIView *)subview atTheRightBottomOfTheView:(UIView *)container offset:(CGSize)offset
+{
+
+}
+
+//中部定位
++ (void)layView:(UIView *)subview atTheTopMiddleOfTheView:(UIView *)container offset:(float)offset
+{
+
+}
++ (void)layView:(UIView *)subview atTheBottomMiddleOfTheView:(UIView *)container offset:(float)offset
+{
+
+}
 
 
+//根据类型，外部定位
++ (void)layView:(UIView *)sourceView toTheRightOfTheView:(UIView *)targetView span:(float)span
+{
+
+}
++ (void)layView:(UIView *)sourceView toTheRightOfTheView:(UIView *)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType
+{
+
+}
 
 
-//+ (void)layView:(UIView *)view atCenterOfView:(UIView *)superView maxSize:(CGSize)size margins:(float)margins
-//{
-//    [superView addSubview:view];
-//    [view mas_makeConstraints:^(MASConstraintMaker *make) {
-//        if (CGSizeEqualToSize(size, CGSizeZero)){
-//            if (margins > 0) {
-//                make.edges.equalTo(superView).insets(UIEdgeInsetsMake(margins, margins, margins, margins));
-//            }else{
-//                make.edges.equalTo(superView).insets(UIEdgeInsetsZero);
-//            }
-//        }else{
-//            make.center.equalTo(superView);
-//            make.size.mas_greaterThanOrEqualTo(size);
-//        }
-//    }];
-//}
++ (void)layView:(UIView *)sourceView toTheLeftOfTheView:(UIView *)targetView span:(float)span
+{
 
-//+ (void)placeView:(UIView *)sourceView atLeftTopOfTheView:(UIView *)targetView maxSize:(CGSize)size offset:(CGSize)offset inner:(BOOL)inner
-//{
-//    [sourceView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.size.mas_greaterThanOrEqualTo(size);
-//        CGFloat finalX = !inner ? -(offset.width+size.width) : offset.width;
-//        CGFloat finalY = !inner ? -(offset.height+size.height) : offset.height;
-//        make.left.equalTo(targetView).with.offset(finalX);
-//        make.top.equalTo(targetView).with.offset(finalY);
-//    }];
-//}
-//
-//+ (void)placeView:(UIView *)sourceView atLeftMiddleOfTheView:(UIView *)targetView maxSize:(CGSize)size offset:(CGSize)offset inner:(BOOL)inner
-//{
-//    [sourceView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.size.mas_greaterThanOrEqualTo(size);
-//        make.centerY.equalTo(targetView);
-//        CGFloat finalX = !inner ? -(offset.width+size.width) : offset.width;
-//        make.left.equalTo(targetView).with.offset(finalX);
-//    }];
-//}
-//
-//+ (void)placeView:(UIView *)sourceView atLeftBottomOfTheView:(UIView *)targetView maxSize:(CGSize)size offset:(CGSize)offset inner:(BOOL)inner
-//{
-//    [sourceView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.size.mas_greaterThanOrEqualTo(size);
-//        CGFloat finalX = !inner ? -(offset.width+size.width) : offset.width;
-//        CGFloat finalY = !inner ? (offset.height+size.height) : offset.height;
-//        make.left.equalTo(targetView).with.offset(finalX);
-//        make.bottom.equalTo(targetView).with.offset(finalY);
-//    }];
-//}
-//
-//+ (void)placeView:(UIView *)sourceView atRightTopOfTheView:(UIView *)targetView maxSize:(CGSize)size offset:(CGSize)offset inner:(BOOL)inner
-//{
-//    [sourceView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.size.mas_greaterThanOrEqualTo(size);
-//        CGFloat finalX = !inner ? (offset.width+size.width) : offset.width;
-//        CGFloat finalY = !inner ? -(offset.height+size.height) : offset.height;
-//        make.right.equalTo(targetView).with.offset(finalX);
-//        make.top.equalTo(targetView).with.offset(finalY);
-//    }];
-//}
-//
-//+ (void)placeView:(UIView *)sourceView atRightMiddleOfTheView:(UIView *)targetView maxSize:(CGSize)size offset:(CGSize)offset inner:(BOOL)inner
-//{
-//    [sourceView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.size.mas_greaterThanOrEqualTo(size);
-//        make.centerY.equalTo(targetView);
-//        CGFloat finalX = !inner ? (offset.width+size.width) : offset.width;
-//        make.right.equalTo(targetView).with.offset(finalX);
-//    }];
-//}
-//
-//+ (void)placeView:(UIView *)sourceView atRightBottomOfTheView:(UIView *)targetView maxSize:(CGSize)size offset:(CGSize)offset inner:(BOOL)inner
-//{
-//    [sourceView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.size.mas_greaterThanOrEqualTo(size);
-//        CGFloat finalX = !inner ? (offset.width+size.width) : offset.width;
-//        CGFloat finalY = !inner ? (offset.height+size.height) : offset.height;
-//        make.right.equalTo(targetView).with.offset(finalX);
-//        make.bottom.equalTo(targetView).with.offset(finalY);
-//    }];
-//}
-//
-//+ (void)placeView:(UIView *)sourceView atTopMiddleOfTheView:(UIView *)targetView maxSize:(CGSize)size offset:(CGSize)offset inner:(BOOL)inner
-//{
-//    [sourceView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.size.mas_greaterThanOrEqualTo(size);
-//        make.centerX.equalTo(targetView);
-//        CGFloat finalY = !inner ? -(offset.height+size.height) : offset.height;
-//        make.top.equalTo(targetView).with.offset(finalY);
-//    }];
-//}
-//
-//+ (void)placeView:(UIView *)sourceView atBottomMiddleOfTheView:(UIView *)targetView maxSize:(CGSize)size offset:(CGSize)offset inner:(BOOL)inner
-//{
-//    [sourceView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.size.mas_greaterThanOrEqualTo(size);
-//        make.centerX.equalTo(targetView);
-//        CGFloat finalY = !inner ? (offset.height+size.height) : -offset.height;
-//        make.bottom.equalTo(targetView).with.offset(finalY);
-//    }];
-//}
-//
+}
++ (void)layView:(UIView *)sourceView toTheLeftOfTheView:(UIView *)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType
+{
+
+}
+
+
++ (void)layView:(UIView *)sourceView aboveTheView:(UIView *)targetView span:(float)span
+{
+
+}
++ (void)layView:(UIView *)sourceView aboveTheView:(UIView *)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType
+{
+
+}
+
+
++ (void)layView:(UIView *)sourceView belowTheView:(UIView *)targetView span:(float)span
+{
+
+}
++ (void)layView:(UIView *)sourceView belowTheView:(UIView *)targetView span:(float)span alignmentType:(FSLayoutAlignmentType)alignmentType
+{
+
+}
+
 
 @end
