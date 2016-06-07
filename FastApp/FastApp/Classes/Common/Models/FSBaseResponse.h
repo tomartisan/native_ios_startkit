9ff25143e84d5af35ab352c235e9288e4ed77ade
tangkunyin <sir@shuoit.net>
2016-06-07 14:59:57 +0800

    增加通用响应实体类，优化服务器返回值处理

---
+++ FastApp/FastApp/Classes/Common/Models/FSBaseResponse.h
@@ -0,0 +1,13 @@
+//
+//  FSBaseResponse.h
+//  FastApp
+//
+//  Created by tangkunyin on 16/6/7.
+//  Copyright © 2016年 www.shuoit.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FSBaseResponse : NSObject
+
+@end
---
+++ FastApp/FastApp/Classes/Common/Models/FSBaseResponse.m
@@ -0,0 +1,13 @@
+//
+//  FSBaseResponse.m
+//  FastApp
+//
+//  Created by tangkunyin on 16/6/7.
+//  Copyright © 2016年 www.shuoit.net. All rights reserved.
+//
+
+#import "FSBaseResponse.h"
+
+@implementation FSBaseResponse
+
+@end
