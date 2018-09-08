8fcba1c46786b9a217be5f0608876e4a31152bc0
tangkunyin <iam@tangkunyin.com>
2018-09-08 11:22:23 +0800

    add shoutdownjs and markdown viewer support

---
+++ FastApp/Classes/Common/Category/UIView+FSMarkDownView.h
@@ -0,0 +1,14 @@
+//
+//  UIView+FSMarkDownView.h
+//  FastApp
+//
+//  Created by tangkunyin on 2018/9/8.
+//  Copyright © 2018年 www.shuoit.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// 支持MarkDown内容预览的视图
+@interface UIView (FSMarkDownView)
+
+@end
---
+++ FastApp/Classes/Common/Category/UIView+FSMarkDownView.m
@@ -0,0 +1,13 @@
+//
+//  UIView+FSMarkDownView.m
+//  FastApp
+//
+//  Created by tangkunyin on 2018/9/8.
+//  Copyright © 2018年 www.shuoit.net. All rights reserved.
+//
+
+#import "UIView+FSMarkDownView.h"
+
+@implementation UIView (FSMarkDownView)
+
+@end
